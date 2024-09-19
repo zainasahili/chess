436cfabdfc23a6477fa6048fc946ea5f48c07bb2
zaina al-sahili <zsahili@byu.edu>
2024-09-18 22:21:51 -0600

    change

---
+++ shared/src/main/java/pieces_rules/Pawnrules.java
@@ -0,0 +1,44 @@
+package pieces_rules;
+
+import chess.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+public class Pawnrules implements Piecesrules{
+
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        ChessGame.TeamColor color = board.getPiece(myPosition).getTeamColor();
+
+        int step = (color == ChessGame.TeamColor.WHITE ? 1 : -1);
+
+        // if pawn is at starting line(2 steps)
+        if ((color == ChessGame.TeamColor.BLACK && myPosition.getRow() == 7)
+                || (color == ChessGame.TeamColor.WHITE && myPosition.getRow() == 2)) {
+
+            ChessPosition newPosition = new ChessPosition(myPosition.getRow() + (2 * step), myPosition.getColumn());
+            if (board.getPiece(newPosition) == null)
+                moves.add(new ChessMove(myPosition, newPosition, board.getPiece(myPosition).getPieceType()));
+        }
+        // if pawn is not at starting line or starting(1 step)
+        int row = myPosition.getRow() + step;
+        if ((color == ChessGame.TeamColor.BLACK && myPosition.getRow() >= 1) ||
+            (color == ChessGame.TeamColor.WHITE && myPosition.getRow() <= 8)) {
+            ChessPosition newPosition = new ChessPosition(row, myPosition.getColumn());
+            if (board.getPiece(newPosition) == null)
+                moves.add(new ChessMove(myPosition, newPosition, board.getPiece(myPosition).getPieceType()));
+        }
+        // if there's an enemy diagonally
+        int col = myPosition.getColumn() + step;
+        ChessPosition newPosition = new ChessPosition(row, col);
+        if (board.getPiece(newPosition) != null && board.getPiece(newPosition).getTeamColor() != color)
+            moves.add(new ChessMove(myPosition, newPosition, board.getPiece(newPosition).getPieceType()));
+
+
+        // if pawn is at then edge of the board
+        return moves;
+    }
+}
