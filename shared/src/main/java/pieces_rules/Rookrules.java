c0bc7cad44e73d980d016d7bdcb4ac6ebedb369a
zaina al-sahili <zsahili@byu.edu>
2024-09-19 22:29:14 -0600

    change

--- shared/src/main/java/pieces_rules/Rookrules.java
+++ shared/src/main/java/pieces_rules/Rookrules.java
@@ -13,28 +13,7 @@ public class Rookrules implements Piecesrules{
         ArrayList<ChessMove> moves = new ArrayList<>();
         ChessGame.TeamColor color = board.getPiece(myPosition).getTeamColor();
 
-        for (int i = -7; i <= 7; i++) {
-            for (int j = -7; j <= 7; j++) {
-                int row = myPosition.getRow() + i;
-                int col = myPosition.getColumn() + j;
-                if( row <= 8 && row >= 1 && 8 >= col && col >= 1 &&
-                        (row == myPosition.getRow() || col == myPosition.getColumn()) ) {
-                    ChessPosition newPosition = new ChessPosition(row, col);
-                    ChessPiece piece = board.getPiece(newPosition);
-                    if (piece == null)
-                        moves.add(new ChessMove(myPosition, newPosition, board.getPiece(myPosition).getPieceType()));
-
-                    else{
-                        if (piece.getTeamColor() != board.getPiece(myPosition).getTeamColor())
-                            moves.add(new ChessMove(myPosition, newPosition, board.getPiece(myPosition).getPieceType()));
-                        break;
-
-                    }
-
-                }
-
-            }
-        }
+        //
         return moves;
     }
 }
