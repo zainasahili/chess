fda7bf9ce2a8eb764eddbc6cdbf67ce4f8ca9943
zaina al-sahili <zsahili@byu.edu>
2024-10-20 15:47:10 -0600

    change

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,13 +1,34 @@
-//package service;
-//
-//
-//import dataaccess.DataAccessException;
-//import model.AuthData;
-//import model.UserData;
-//
-//public class UserService {
+package service;
+
+
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+public class UserService {
+        UserDAO userDAO;
+        GameDAO gameDAO;
+        AuthDAO authDAO;
+
+    public UserService(UserDAO userDAO, GameDAO gameDAO, AuthDAO authDAO) {
+        this.userDAO = userDAO;
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+    }
+
+
 //    public AuthData createUser(UserData user) {
+//
 //    }
 //    public AuthData login(UserData user) {}
 //    public void logout(AuthData auth) {}
-//}
+    public void clear(){
+        userDAO.clear();
+        gameDAO.clear();
+        authDAO.clear();
+    }
+}
