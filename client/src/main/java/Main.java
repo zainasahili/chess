7cc75b80aceefb9e2d942c18abc714d28ae19d14
zaina al-sahili <zsahili@byu.edu>
2024-12-08 19:28:34 -0700

    phase 6

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Server server = new Server();
         int port = server.run(0);
-        ServerFacade facade = new ServerFacade("localhost:" + port);
+        ServerFacade facade = new ServerFacade("localhost:8080");
         System.out.println("Welcome to 240 chess. Type Help to get started.");
         PreLogin preLogin = new PreLogin(facade);
         preLogin.run();
